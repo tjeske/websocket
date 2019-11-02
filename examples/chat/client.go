6d3230341deb6d2ccd16bc9758596103cb68a134
Tobias Jeske <tobias.jeske@itemis.com>
2019-11-02 10:33:05 +0100

    allow all

--- examples/chat/client.go
+++ examples/chat/client.go
@@ -122,6 +122,7 @@ func (c *Client) writePump() {
 
 // serveWs handles websocket requests from the peer.
 func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
